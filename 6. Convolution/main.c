6a4721eab7ee0716116ed56ff6a79c16030e366d
vaguebrownfox <foxjournal.nsfw@gmail.com>
2021-09-25 13:21:26 +0530

    stdout log

--- 6. Convolution/main.c
+++ 6. Convolution/main.c
@@ -7,8 +7,8 @@
 extern double InputSignal_f32_1kHz_15kHz[SIG_LENGTH];
 extern double Impulse_response[IMP_RSP_LENGTH];
 
+void write_signal_to_file(char *filename, double *signal_arr, int sig_length);
 void convolution(double *sig_src_arr, double *sig_dest_arr, double *imp_resp_arr, int sig_src_length, int imp_resp_length);
-void write_to_file(char *filename, double *signal_arr, int sig_length);
 
 int main()
 {
@@ -22,32 +22,14 @@ int main()
                 (int)SIG_LENGTH,
                 (int)IMP_RSP_LENGTH);
 
-    // FILE *input_sig_fptr;
-    // FILE *impulse_sig_fptr;
-    // FILE *output_sig_fptr;
-
-    // input_sig_fptr = fopen("input_signal.dat", "w");
-    // impulse_sig_fptr = fopen("impulse_sig.dat", "w");
-
-    // for (int i = 0; i < SIG_LENGTH; i++)
-    // {
-    //     fprintf(input_sig_fptr, "\n%f", InputSignal_f32_1kHz_15kHz[i]);
-    // }
-    // fclose(input_sig_fptr);
-
-    // for (int i = 0; i < IMP_RSP_LENGTH; i++)
-    // {
-    //     fprintf(impulse_sig_fptr, "\n%f", Impulse_response[i]);
-    // }
-    // fclose(impulse_sig_fptr);
-    write_to_file("input_sig.dat", (double *)&InputSignal_f32_1kHz_15kHz[0], SIG_LENGTH);
-    write_to_file("impulse_response.dat", (double *)&Impulse_response[0], IMP_RSP_LENGTH);
-    write_to_file("convoluted_sig.dat", (double *)&OutputSignal[0], out_sig_length);
+    write_signal_to_file("input_sig.dat", (double *)&InputSignal_f32_1kHz_15kHz[0], SIG_LENGTH);
+    write_signal_to_file("impulse_response.dat", (double *)&Impulse_response[0], IMP_RSP_LENGTH);
+    write_signal_to_file("convoluted_sig.dat", (double *)&OutputSignal[0], out_sig_length);
 
     return 0;
 }
 
-void write_to_file(char *filename, double *signal_arr, int sig_length)
+void write_signal_to_file(char *filename, double *signal_arr, int sig_length)
 {
     FILE *signal_fptr;
     signal_fptr = fopen(filename, "w");
@@ -57,4 +39,6 @@ void write_to_file(char *filename, double *signal_arr, int sig_length)
         fprintf(signal_fptr, "\n%f", signal_arr[i]);
     }
     fclose(signal_fptr);
+
+    printf("\nfinished writing file: %s", filename);
 }
